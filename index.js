5446f9921ed8f754abc2e340b931ffb86d77ebec
seaweed2 <winseawoo@naver.com>
2024-08-29 18:01:56 +0900

    서버 설정

--- index.js
+++ index.js
@@ -1,38 +1,15 @@
 // ESM (ES6 Module, Ecma Script Module)     import, export
 // CommonJS Module : require, module.exports 또는 exports
-
 // const express = require('express')
-
+import dotenv from "dotenv";
+dotenv.config();
 import express from "express";
-
 const app = express()
 
-app.get('/', function (req, res) {
-  res.send('GET 요청!')
-});
+import userRouter from "./routes/userRouter.js";
 
-app.post('/', function(req, res){
-    res.send('POST 요청!')
-});
-
-app.put('/', function(req, res){
-    res.send('PUT 요청!')
-});
-
-app.delete('/', function(req, res){
-    res.send('DELETE 요청!')
-});
-
+// middleware
+app.use(express.json()); // json 데이터로 express에서 처리 가능
+app.use("/", userRouter);
 
 app.listen(3000)
-
-// ORM 이해하기, 시퀄라이즈
-
-// 옛날 방식
-// import http from "http"
-
-// http.createServer((req, res) => {
-//     res.write("Hello World!")
-// }).listen(3000, () => {
-//     console.log('server is running on 3000 port!');
-// })
