0123b0a7611f2c9f3c32bbba113d33569a5c60c6
seaweed2 <winseawoo@naver.com>
2024-08-30 18:05:49 +0900

    user 라우터 완료

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -10,7 +10,18 @@ const timeLog = (req, res, next) => {
 router.use(timeLog)
 
 // User(연락처) 생성
-router.post('/', userControl.createUser);
+router.post('/user', userControl.createUser);
+
+// User(연락처) 조회
+router.get('/user/:id', userControl.findOneUser); // 동적파라미터
+router.get('/users', userControl.findAllUsers);
+
+// 연락처 삭제
+router.delete('/users', userControl.removeAllUsers)
+router.delete('/user/:id', userControl.removeUser)
+
+// 연락처 수정 (put / patch)
+router.patch('/user', userControl.updateUser);
 
 //module.exports = router; // CommonJS 내보내기 문법 --> 현재는, ESM 방식
 export default router;
